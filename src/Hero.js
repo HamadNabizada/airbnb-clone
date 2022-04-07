7435afe1ea83d940df417d7bec034dc6412bd13a
Hamad <HamadNabizada@gmail.com>
2022-04-06 22:38:53 -0700

    header completed

---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero(){
+    <div className="hero-wrapper">
+        <div className="img-container"></div>
+        <div className="hero-text-container">
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    </div>
+}
